7d3c02286fafe46dcfd0a858728092d81f083fa9
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-09-18 17:33:33 -0600

    add configurando datos de página vacante show

---
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -0,0 +1,13 @@
+<div class="p-10">
+    <div class="mb-5">
+        <h3 class="font-bold text-3xl text-gray-500 my-3">
+            {{ $vacante->titulo }}
+        </h3>
+
+        <div >
+            <p class="font-bold text-sm uppercase text-gray-800 my-3">Empresa: 
+                <span>{{ $vacante->empresa }}</span>
+            </p>
+        </div>
+    </div>
+</div>
