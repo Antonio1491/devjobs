27a223d7c321ead37efab93d9829a3a4e6271c0f
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-11-06 17:13:11 -0600

    agregando directiva @can en la barra de navegaión

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,15 +11,17 @@
                 </div>
 
                 @auth    
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
-                            {{ __('Mis Vacantes') }}
-                        </x-nav-link>
-                        <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
-                            {{ __('Crear Vacante') }}
-                        </x-nav-link>
-                    </div>
+                    @can('create', App\Models\Vacante::class)
+                        <!-- Navigation Links -->
+                        <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                            <x-nav-link :href="route('vacantes.index')" :active="request()->routeIs('vacantes.index')">
+                                {{ __('Mis Vacantes') }}
+                            </x-nav-link>
+                            <x-nav-link :href="route('vacantes.create')" :active="request()->routeIs('vacantes.create')">
+                                {{ __('Crear Vacante') }}
+                            </x-nav-link>
+                        </div>
+                    @endcan
                 @endauth                
 
             </div>
@@ -27,11 +29,12 @@
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 @auth
-                    @if(auth()->user()->rol === 2)
+                    {{-- si el usuario puede crear vacantes --}}
+                    @can('create', App\Models\Vacante::class)
                         <a href="{{ route('notificaciones') }}" class="mr-2 w-7 h-7 bg-indigo-600 hover:bg-indigo-800 rounded-full flex flex-col justify-center items-center text-white font-extrabold text-sm">
                             {{ auth()->user()->unreadNotifications->count() }}
                         </a>
-                    @endif
+                    @endcan
 
 
                     <x-dropdown align="right" width="48">
