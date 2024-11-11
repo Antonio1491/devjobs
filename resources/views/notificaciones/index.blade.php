29660292ac5bc7ece4606514ec5c499e223128ed
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-11-11 17:09:13 -0600

    ordenando desc a los candidatos

--- resources/views/notificaciones/index.blade.php
+++ resources/views/notificaciones/index.blade.php
@@ -11,28 +11,29 @@
               <div class="p-6 text-gray-900 dark:text-gray-100">
                 <h1 class="text-2xl font-bold mb-10 text-center">Mis Notificaciones</h1>
                 
-                @forelse ($notificaciones as $notificacion)
-                  <div class="p-5 border border-gray-200 lg:flex lg:justify-between lg:items-center">
-                    <div>
-                      <p>Tienes un nuevo candidato en:
-                        <span class="font-bold">
-                          {{ $notificacion->data['nombre_vacante'] }}
-                        </span>
-                      </p>
-                      <p>
-                        <span class="font-bold">
-                          {{ $notificacion->created_at->diffForHumans() }}
-                        </span>
-                      </p>
+                <div class="divede-y divay-gray-200">
+                  @forelse ($notificaciones as $notificacion)
+                    <div class="p-5 lg:flex lg:justify-between lg:items-center">
+                      <div>
+                        <p>Tienes un nuevo candidato en:
+                          <span class="font-bold">
+                            {{ $notificacion->data['nombre_vacante'] }}
+                          </span>
+                        </p>
+                        <p>
+                          <span class="font-bold">
+                            {{ $notificacion->created_at->diffForHumans() }}
+                          </span>
+                        </p>
+                      </div>
+                      <div class="mt-5 lg:mt-0">
+                        <a href="{{ route('candidatos.index', $notificacion->data['id_vacante']) }}" class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white">Ver candidatos</a>
+                      </div>
                     </div>
-                    <div class="mt-5 lg:mt-0">
-                      <a href="#" class="bg-indigo-500 p-3 text-sm uppercase font-bold text-white">Ver candidatos</a>
-                    </div>
-                  </div>
-                @empty
-                  <p class="text-center text-gray-600 ">No Hay Notificaciones Nuevas</p>
-                @endforelse
-
+                  @empty
+                    <p class="text-center text-gray-600 ">No Hay Notificaciones Nuevas</p>
+                  @endforelse
+                </div>
               </div>
           </div>
       </div>
