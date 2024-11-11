950013f18fc383aa286baa03ab1e126461f44f9d
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-11-11 16:13:54 -0600

    creamos la vista de candidatos

---
+++ resources/views/candidatos/index.blade.php
@@ -0,0 +1,40 @@
+<x-app-layout>
+  <x-slot name="header">
+      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+          {{ __('Candidatos Vacante') }}
+      </h2>
+  </x-slot>
+
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+          <div class="p-6 text-gray-900 dark:text-gray-100">
+            <h1 class="text-2xl font-bold mb-10 text-center">
+              Candidatos Vacante: {{ $vacante->titulo }}
+            </h1>
+            
+            <div class="md:flex md:justify-center p-5">
+              
+                <ul class="divide-y divide-gray-100">
+                  @forelse ( $vacante->candidatos as $candidato )
+                    <li class="p-3 flex items-center">
+                      <div class="flex-1">
+                        {{-- <pre>
+                          {{ $candidato }}
+                        </pre> --}}
+                      </div>
+                      <div>
+
+                      </div>
+                    </li>
+                  @empty
+                    <p class="p-3 text-center text-sm text-gray-300">No hay candidatos aún</p>
+                  @endforelse
+                </ul>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</x-app-layout>
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -18,7 +18,7 @@
 
                     <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
                         <a 
-                            href=""
+                            href="{{ route('candidatos.index', $vacante) }}"
                             class="bg-slate-600 py-2 px-4 rounded-lg text-white text-xs font-bold text-center uppercase"
                         >Candidatos</a>
                         <a 
