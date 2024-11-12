561bd04ef44a16c0706e64448490620e3fb799c9
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-11-12 16:53:59 -0600

    creamos el componente home-vacantes de livewire

---
+++ app/Livewire/Homevacantes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Homevacantes extends Component
+{
+    public function render()
+    {
+        return view('livewire.home-vacantes');
+    }
+}
