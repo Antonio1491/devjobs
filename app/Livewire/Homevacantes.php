1376f410ca596ac3215bee78cb80aa43f7b41647
Antonio De J. Góngora García <antonio.gongora1491@gmail.com>
2024-11-13 17:08:47 -0600

    leyendo los datos ingresados en el formulario

--- app/Livewire/Homevacantes.php
+++ app/Livewire/Homevacantes.php
@@ -7,12 +7,25 @@ use Livewire\Component;
 
 class Homevacantes extends Component
 {
+    public $termino;
+    public $categoria;
+    public $salario;
+
+    protected $listeners = ['terminosBusqueda' => 'buscar'];
+
+    public function buscar( $termino, $categoria, $salario)
+    {
+        $this->termino = $termino;
+        $this->termino = $categoria;
+        $this->termino = $salario;
+
+        dd($this->termino);
+    }
+
     public function render()
     {
 
         $vacantes = Vacante::all();
-
-
 
         return view('livewire.home-vacantes',[
             'vacantes' => $vacantes
